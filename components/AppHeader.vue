4d4eca576309d78d58b3f6086ef77db438fac62f
shuvo-me <shuvo.js.dev@gmail.com>
2023-12-08 07:56:38 +0600

    initial commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="antialiased">
+    <div class="container p-4">
+      <nav class="">
+        <div>
+          <h4 class="text-xl text-gray-900 font-bold">
+            Product Barcode Project With Nuxtjs
+          </h4>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <main class="container p-10">
+      <slot />
+    </main>
+  </div>
+</template>
